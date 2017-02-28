bef3fead9984a020caf286e2849e89a6e8a1d991
Bhramar <bhatnagarbhramar@gmail.com>
2017-02-28 17:04:58 +0530

    Changes for Python3 and SVM Mini Project
    SVM Mini Project Commit

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,7 +24,41 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+print("BEFORE----")
+print("len(features_train)")
+print(len(features_train))
+print("len(labels_train)")
+print(len(labels_train))
+#
+#features_train = features_train[:len(features_train)//100] 
+#labels_train = labels_train[:len(labels_train)//100] 
+#
+#print("AFTER------")
+#print("len(features_train)")
+#print(len(features_train))
+#print("len(labels_train)")
+#print(len(labels_train))
 
+from sklearn.svm import SVC
+clf = SVC(kernel="rbf",C=10000.0)
+t0=time()
+clf.fit(features_train,labels_train)
+print("Time to train SVC(Kernel=RBF,C=10000.0):",round(time()-t0,4),"s")
+
+t1=time()
+pred=clf.predict(features_test)
+print("Time to predict SVC(Kernel=RBF,C=10000.0):",round(time()-t1,4),"s")
+
+print ("Prediction for element ")
+print (pred)
+
+count = (pred == 1).sum()
+print(" cris count " )
+print (count)
+
+accuracy = clf.score(features_test,labels_test)
+print ("accuracy")
+print (accuracy)
 #########################################################
 
 
