590ceb35e8e3ffccec74321b163a84adb95f9546
Bhramar <bhatnagarbhramar@gmail.com>
2017-03-02 09:41:00 +0530

    DT changes
    Decision Tree Changes

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -25,6 +25,25 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+feat = len(features_train[0])
+print("features",feat)
+
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+t0=time()
+clf = clf.fit(features_train,labels_train)
+print("Traing time",round(time()-t0,4), "s")
+
+acc = clf.score(features_test,labels_test)
+print("Accuracy:", str(acc))
+
+## Output :
+    
+#no. of Chris training emails: 7936
+#no. of Sara training emails: 7884
+#features 379
+#Traing time 4.6321 s
+#Accuracy: 0.966439135381
 
 #########################################################
 
